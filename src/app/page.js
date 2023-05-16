d2e64322e408212f17aabd99c021708ad31cb602
Faizan <faizyboy1@gmail.com>
2023-05-16 10:39:18 +0500

    data fetching tmdb

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
+import Results from "@/components/Results"
 
-
-export default function Home() {
+const API_KEY = process.env.API_KEY
+export default async function Home({searchParams}) {
+  const genre= searchParams.genre || 'fetchTrending'
+  const res = await fetch(`https://api.themoviedb.org/3/${genre ==='fetchTopRated'?'movie/top_rated':'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:1000*10}})
+  console.log({res});
+  if(!res.ok) throw new Error("Failed to fetch the data")
+  const {results}= await res.json()
+  console.log({results})
   return (
-      <h1 className='text-red-600'>HOME</h1>
+      <div>
+        <Results results={results} />
+      </div>
       )
 }
